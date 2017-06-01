9fcf0527defa3d1834b785f421316d1c8e383c4a
Geert van Horrik <geert@catenalogic.com>
2017-06-01 16:57:28 +0200

    Add benchmarks for PropertyBag

---
+++ src/Catel.Benchmarks.Shared/Data/PropertyBag/PropertyBag_GetPropertyValue_Benchmark.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PropertyBag_GetPropertyValue_Benchmark.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2017 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel.Benchmarks.Data.PropertyBag
+{
+    using BenchmarkDotNet.Attributes;
+    using Catel.Data;
+
+    public class PropertyBag_GetPropertyValue_Benchmark
+    {
+        #region Fields
+        private readonly PropertyBag _propertyBag;
+        #endregion
+
+        public PropertyBag_GetPropertyValue_Benchmark()
+        {
+            _propertyBag = new PropertyBag();
+            _propertyBag["string"] = "available";
+            _propertyBag["int"] = 42;
+            _propertyBag["bool"] = true;
+        }
+
+        #region Methods
+        [Benchmark]
+        public string GetStringValue()
+        {
+            var value = _propertyBag.GetPropertyValue("string", "default");
+            return value;
+        }
+
+        [Benchmark]
+        public int GetIntValue()
+        {
+            var value = _propertyBag.GetPropertyValue("int", 0);
+            return value;
+        }
+
+        [Benchmark]
+        public bool GetBoolValue()
+        {
+            var value = _propertyBag.GetPropertyValue("bool", false);
+            return value;
+        }
+        #endregion
+    }
+}
---
+++ src/Catel.Benchmarks.Shared/Data/PropertyBag/PropertyBag_SetPropertyValue_Benchmark.cs
@@ -0,0 +1,40 @@
+﻿
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PropertyBag_GetPropertyValue_Benchmark.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2017 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel.Benchmarks.Data.PropertyBag
+{
+    using BenchmarkDotNet.Attributes;
+    using Catel.Data;
+
+    public class PropertyBag_SetPropertyValue_Benchmark
+    {
+        #region Fields
+        private readonly PropertyBag _propertyBag = new PropertyBag();
+        #endregion
+
+        #region Methods
+        [Benchmark]
+        public void SetStringValue()
+        {
+            _propertyBag.SetPropertyValue("string", "available");
+        }
+
+        [Benchmark]
+        public void SetIntValue()
+        {
+            _propertyBag.SetPropertyValue("int", 42);
+        }
+
+        [Benchmark]
+        public void SetBoolValue()
+        {
+            _propertyBag.SetPropertyValue("string", true);
+        }
+        #endregion
+    }
+}
