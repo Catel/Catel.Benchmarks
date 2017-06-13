0cf4a777f64cfd32d970b2c49ba8e2b911ffd7cc
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2017-06-12 22:48:54 -0400

    (+) CTL-973 Implement Enum<T> benchmark

---
+++ src/Catel.Benchmarks.Shared/Enum/Enum_Benchmark.cs
@@ -0,0 +1,101 @@
+﻿namespace Catel.Benchmarks.Enum
+{
+    using BenchmarkDotNet.Attributes;
+
+    public class Enum_Benchmark : BenchmarkBase
+    {
+        #region Methods
+        [Benchmark]
+        public void ToList()
+        {
+            Enum<EnumForBenchmarkA>.ToList();
+        }
+
+        [Benchmark]
+        public void ConvertFromOtherEnumValue()
+        {
+            Enum<EnumForBenchmarkA>.ConvertFromOtherEnumValue(EnumForBenchmarkB.A);
+        }
+
+        [Benchmark]
+        public void GetName()
+        {
+            Enum<EnumForBenchmarkA>.GetName(0);
+        }
+
+        [Benchmark]
+        public void GetName_Long()
+        {
+            Enum<EnumForBenchmarkA>.GetName((long) 0);
+        }
+
+        [Benchmark]
+        public void GetNames()
+        {
+            Enum<EnumForBenchmarkA>.GetNames();
+        }
+
+        [Benchmark]
+        public void GetValues()
+        {
+            Enum<EnumForBenchmarkA>.GetValues();
+        }
+
+        [Benchmark]
+        public void Parse()
+        {
+            Enum<EnumForBenchmarkA>.Parse("A");
+        }
+
+        [Benchmark]
+        public void TryParse_Nullable()
+        {
+            EnumForBenchmarkA? value;
+            Enum<EnumForBenchmarkA>.TryParse("A", out value);
+        }
+
+        [Benchmark]
+        public void TryParse()
+        {
+            EnumForBenchmarkA value;
+            Enum<EnumForBenchmarkA>.TryParse("A", out value);
+        }
+
+        [Benchmark]
+        public void TryParse_IgnoreCase_Nullable()
+        {
+            EnumForBenchmarkA? value;
+            Enum<EnumForBenchmarkA>.TryParse("a", true, out value);
+        }
+
+        [Benchmark]
+        public void TryParse_IgnoreCase()
+        {
+            EnumForBenchmarkA value;
+            Enum<EnumForBenchmarkA>.TryParse("a", true, out value);
+        }
+        #endregion
+
+        #region Nested type: EnumForBenchmarkA
+        private enum EnumForBenchmarkA
+        {
+            A,
+            B,
+            C,
+            D,
+            E
+        }
+        #endregion
+
+        #region Nested type: EnumForBenchmarkB
+        private enum EnumForBenchmarkB
+        {
+            A,
+            B,
+            C,
+            D,
+            E
+        }
+        #endregion
+    }
+}
