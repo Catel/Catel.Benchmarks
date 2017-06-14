e36021ffb88b30e18667db2ec9c3204af64c3e14
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2017-06-13 23:12:49 -0400

    (+) CTL-970 Create benchmark for ScopeManager

---
+++ src/Catel.Benchmarks.Shared/Scoping/ScopeManager_Benchmark.cs
@@ -0,0 +1,28 @@
+﻿namespace Catel.Benchmarks.Scoping
+{
+    using BenchmarkDotNet.Attributes;
+    using Catel.Scoping;
+
+    public class ScopeManager_Benchmark : BenchmarkBase
+    {
+        [Setup]
+        public void Init()
+        {
+            ScopeManager<object>.GetScopeManager("stored");
+        }
+
+        #region Methods
+        [Benchmark]
+        public void Get_New_ScopeManager()
+        {
+            ScopeManager<object>.GetScopeManager("scope");
+        }
+
+        [Benchmark]
+        public void Get_Existing_ScopeManager()
+        {
+            ScopeManager<object>.GetScopeManager("stored");
+        }
+        #endregion
+    }
+}
