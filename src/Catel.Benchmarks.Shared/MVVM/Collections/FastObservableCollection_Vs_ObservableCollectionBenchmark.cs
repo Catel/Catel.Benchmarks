8dacc390f9ccf6c2af1eecb342ef105472e02cd9
Geert van Horrik <geert@catenalogic.com>
2017-06-16 19:52:55 +0200

    Build fix

--- src/Catel.Benchmarks.Shared/MVVM/Collections/FastObservableCollection_Vs_ObservableCollectionBenchmark.cs
+++ src/Catel.Benchmarks.Shared/MVVM/Collections/FastObservableCollection_Vs_ObservableCollectionBenchmark.cs
@@ -8,7 +8,10 @@
     public class FastObservableCollectionVsObservableCollectionVsFastBindingListBenchmark : BenchmarkBase
     {
         #region Fields
+#if !CATEL_4_4 && !CATEL_4_3 && !CATEL_4_2
         private FastBindingList<int> _fastBindingList;
+#endif
+
         private FastObservableCollection<int> _fastObservableCollection;
         private ObservableCollection<int> _observableCollection;
         #endregion
@@ -17,16 +20,20 @@
         [Setup]
         public void Init()
         {
-            _observableCollection = new ObservableCollection<int> {-1, -2, -3, -4, -5, -6, -7, -8, -9, -10};
-            _fastObservableCollection = new FastObservableCollection<int> {-1, -2, -3, -4, -5, -6, -7, -8, -9, -10};
-            _fastBindingList = new FastBindingList<int> {-1, -2, -3, -4, -5, -6, -7, -8, -9, -10};
+#if !CATEL_4_4 && !CATEL_4_3 && !CATEL_4_2
+            _fastBindingList = new FastBindingList<int> { -1, -2, -3, -4, -5, -6, -7, -8, -9, -10 };
+#endif
+
+            _observableCollection = new ObservableCollection<int> { -1, -2, -3, -4, -5, -6, -7, -8, -9, -10 };
+            _fastObservableCollection = new FastObservableCollection<int> { -1, -2, -3, -4, -5, -6, -7, -8, -9, -10 };
+
         }
 
         [Benchmark]
         public void ObservableCollection_BashInsert()
         {
 #if !CATEL_4_4 && !CATEL_4_3 && !CATEL_4_2
-            (_observableCollection as ICollection<int>).AddRange(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+            (_observableCollection as ICollection<int>).AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
 #else
             _observableCollection.AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
 #endif
@@ -36,17 +43,19 @@
         public void FastObservableCollection_BashInsert()
         {
 #if !CATEL_4_4 && !CATEL_4_3 && !CATEL_4_2
-            _fastObservableCollection.AddItems(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, SuspensionMode.Adding);
+            _fastObservableCollection.AddItems(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, SuspensionMode.Adding);
 #else
             _fastObservableCollection.AddItems(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
 #endif
         }
 
+#if !CATEL_4_4 && !CATEL_4_3 && !CATEL_4_2
         [Benchmark]
         public void FastBindingList_BashInsert()
         {
-            _fastBindingList.AddItems(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+            _fastBindingList.AddItems(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
         }
+#endif
 
         [Benchmark]
         public void ObservableCollection_BashRemove()
@@ -60,7 +69,11 @@
         [Benchmark]
         public void FastObservableCollection_BashRemove()
         {
+#if !CATEL_4_4 && !CATEL_4_3 && !CATEL_4_2
             using (_fastObservableCollection.SuspendChangeNotifications(SuspensionMode.Removing))
+#else
+            using (_fastObservableCollection.SuspendChangeNotifications())
+#endif
             {
                 for (var i = _fastObservableCollection.Count; i >= 0; i--)
                 {
@@ -69,11 +82,13 @@
             }
         }
 
+#if !CATEL_4_4 && !CATEL_4_3 && !CATEL_4_2
         [Benchmark]
-        public void FastBindingList__BashRemove()
+        public void FastBindingList_BashRemove()
         {
-            _fastBindingList.RemoveItems(new[] {-1, -2, -3, -4, -5, -6, -7, -8, -9, -10});
+            _fastBindingList.RemoveItems(new[] { -1, -2, -3, -4, -5, -6, -7, -8, -9, -10 });
         }
+#endif
         #endregion
     }
 }
