99102a9fb505d2d928e758075023b77256ad3aa4
Igr Alexánder Fernández Saúco <alexander.fernandez@datys.cu>
2017-06-20 11:47:09 -0400

    (+) #117 Add ExpressionHelper Benckmark

---
+++ src/Catel.Benchmarks.Shared/Helpers/ExpressionHelper_Benckmark.cs
@@ -0,0 +1,33 @@
+﻿namespace Catel.Benchmarks.Helpers
+{
+    using BenchmarkDotNet.Attributes;
+
+    public class ExpressionHelper_Benckmark : BenchmarkBase
+    {
+        #region Methods
+        [Benchmark]
+        public void GetPropertyName()
+        {
+            var instance = new ClassA();
+            var propertyName = ExpressionHelper.GetPropertyName(() => instance.PropertyA);
+        }
+
+        [Benchmark]
+        public void GetOwner()
+        {
+            var instance = new ClassA();
+            var owner = ExpressionHelper.GetOwner(() => instance.PropertyA);
+        }
+
+        #endregion
+
+        #region Nested type: A
+        public class ClassA
+        {
+            #region Properties
+            public int PropertyA { get; set; }
+            #endregion
+        }
+        #endregion
+    }
+}
