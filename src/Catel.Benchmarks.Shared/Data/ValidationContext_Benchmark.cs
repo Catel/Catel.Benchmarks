fa2cbd7c7c0dcad2bbcba43844d763bfddb6f77e
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2017-06-21 00:29:53 -0400

    (+) #965 Create benchmarks for ValidationContext / ValidationSummary

---
+++ src/Catel.Benchmarks.Shared/Data/ValidationContext_Benchmark.cs
@@ -0,0 +1,133 @@
+﻿namespace Catel.Benchmarks.Data
+{
+    using System.Collections.Generic;
+    using BenchmarkDotNet.Attributes;
+    using Catel.Data;
+
+    public class ValidationContext_Benchmark : BenchmarkBase
+    {
+        #region Fields
+        private List<IBusinessRuleValidationResult> _businessRuleValidationResult;
+        private List<IFieldValidationResult> _fieldValidationResults;
+        private ValidationContext _validationContext;
+        #endregion
+
+        #region Methods
+        [Setup]
+        public void Init()
+        {
+            _fieldValidationResults = new List<IFieldValidationResult> {FieldValidationResult.CreateError("FieldA", "Field Error"), FieldValidationResult.CreateError("FieldB", "FieldB Error"), FieldValidationResult.CreateWarning("FieldC", "FieldC Warning")};
+            _businessRuleValidationResult = new List<IBusinessRuleValidationResult> {BusinessRuleValidationResult.CreateError("BusinessRules Error"), BusinessRuleValidationResult.CreateWarning("BusinessRules Warning")};
+            _validationContext = new ValidationContext(_fieldValidationResults, _businessRuleValidationResult);
+        }
+
+        [Benchmark]
+        public void Initialization()
+        {
+            var validationContext = new ValidationContext(_fieldValidationResults, _businessRuleValidationResult);
+        }
+
+        [Benchmark]
+        public void HasErrors()
+        {
+            var validationContextHasErrors = _validationContext.HasErrors;
+        }
+
+        [Benchmark]
+        public void HasWarnings()
+        {
+            var validationContextHasWarnings = _validationContext.HasWarnings;
+        }
+
+        [Benchmark]
+        public void GetBusinessRuleErrorCount()
+        {
+            _validationContext.GetBusinessRuleErrorCount();
+        }
+
+        [Benchmark]
+        public void GetFieldErrorCount()
+        {
+            _validationContext.GetFieldErrorCount();
+        }
+
+        [Benchmark]
+        public void GetErrorCount()
+        {
+            _validationContext.GetErrorCount();
+        }
+
+        [Benchmark]
+        public void GetWarningCount()
+        {
+            _validationContext.GetWarningCount();
+        }
+
+        [Benchmark]
+        public void GetFieldWarningCount()
+        {
+            _validationContext.GetFieldWarningCount();
+        }
+
+        [Benchmark]
+        public void GetBusinessRuleWarningCount()
+        {
+            _validationContext.GetBusinessRuleWarningCount();
+        }
+
+        [Benchmark]
+        public void GetValidations()
+        {
+            _validationContext.GetValidations();
+        }
+
+        [Benchmark]
+        public void GetFieldValidations()
+        {
+            _validationContext.GetFieldValidations();
+        }
+
+        [Benchmark]
+        public void GetBusinessRuleValidations()
+        {
+            _validationContext.GetBusinessRuleValidations();
+        }
+
+        [Benchmark]
+        public void GetBusinessRuleErrors()
+        {
+            _validationContext.GetBusinessRuleErrors();
+        }
+
+        [Benchmark]
+        public void GetBusinessRuleWarnings()
+        {
+            _validationContext.GetBusinessRuleWarnings();
+        }
+
+        [Benchmark]
+        public void GetWarnings()
+        {
+            _validationContext.GetWarnings();
+        }
+
+        [Benchmark]
+        public void GetFieldErrors()
+        {
+            _validationContext.GetFieldErrors();
+        }
+
+        [Benchmark]
+        public void GetFieldWarnings()
+        {
+            _validationContext.GetFieldWarnings();
+        }
+
+        [Benchmark]
+        public void GetErrors()
+        {
+            _validationContext.GetErrors();
+        }
+        #endregion
+    }
+}
---
+++ src/Catel.Benchmarks.Shared/Data/ValidationSummary_Benchmark.cs
@@ -0,0 +1,92 @@
+namespace Catel.Benchmarks.Data
+{
+    using System.Collections.Generic;
+    using BenchmarkDotNet.Attributes;
+    using Catel.Data;
+
+    public class ValidationSummary_Benchmark : BenchmarkBase
+    {
+        #region Fields
+        private ValidationContext _validationContext;
+        private ValidationSummary _validationSummary;
+        #endregion
+
+        #region Methods
+        [Setup]
+        public void Init()
+        {
+            var fieldValidationResults = new List<IFieldValidationResult> {FieldValidationResult.CreateError("FieldA", "Field Error"), FieldValidationResult.CreateError("FieldB", "FieldB Error"), FieldValidationResult.CreateWarning("FieldC", "FieldC Warning")};
+            var businessRuleValidationResult = new List<IBusinessRuleValidationResult> {BusinessRuleValidationResult.CreateError("BusinessRules Error"), BusinessRuleValidationResult.CreateWarning("BusinessRules Warning")};
+            _validationContext = new ValidationContext(fieldValidationResults, businessRuleValidationResult);
+
+            _validationSummary = new ValidationSummary(_validationContext);
+        }
+
+        [Benchmark]
+        public void Initialization()
+        {
+            new ValidationSummary(_validationContext);
+        }
+
+        [Benchmark]
+        public void HasWarnings()
+        {
+            var validationSummaryHasWarnings = _validationSummary.HasWarnings;
+        }
+
+        [Benchmark]
+        public void HasErrors()
+        {
+            var validationSummaryHasErrors = _validationSummary.HasErrors;
+        }
+
+        [Benchmark]
+        public void HasFieldErrors()
+        {
+            var validationSummaryHasFieldErrors = _validationSummary.HasFieldErrors;
+        }
+
+        [Benchmark]
+        public void HasFieldWarnings()
+        {
+            var validationSummaryHasFieldWarnings = _validationSummary.HasFieldWarnings;
+        }
+
+        [Benchmark]
+        public void HasBusinessRuleErrors()
+        {
+            var validationSummaryHasBusinessRuleErrors = _validationSummary.HasBusinessRuleErrors;
+        }
+
+        [Benchmark]
+        public void HasBusinessRuleWarnings()
+        {
+            var validationSummaryHasBusinessRuleWarnings = _validationSummary.HasBusinessRuleWarnings;
+        }
+
+        [Benchmark]
+        public void BusinessRuleErrors()
+        {
+            var validationSummaryHasBusinessRuleWarnings = _validationSummary.BusinessRuleErrors;
+        }
+
+        [Benchmark]
+        public void BusinessRuleWarnings()
+        {
+            var validationSummaryBusinessRuleWarnings = _validationSummary.BusinessRuleWarnings;
+        }
+
+        [Benchmark]
+        public void FieldErrors()
+        {
+            var validationSummaryFieldErrors = _validationSummary.FieldErrors;
+        }
+
+        [Benchmark]
+        public void FieldWarnings()
+        {
+            var validationSummaryFieldWarnings = _validationSummary.FieldWarnings;
+        }
+        #endregion
+    }
+}
