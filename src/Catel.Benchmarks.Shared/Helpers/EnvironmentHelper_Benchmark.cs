0f9a58116909daa960b0b7fb2dd006ec23c6f4fd
Geert van Horrik <geert@catenalogic.com>
2017-06-27 08:30:19 +0200

    Run all benchmarks

--- src/Catel.Benchmarks.Shared/Helpers/EnvironmentHelper_Benchmark.cs
+++ src/Catel.Benchmarks.Shared/Helpers/EnvironmentHelper_Benchmark.cs
@@ -5,60 +5,62 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 
-namespace Catel.Benchmarks
-{
-    using BenchmarkDotNet.Attributes;
+// Excluded since these benchmarks take a very long time to check actual processes
 
-    public class EnvironmentHelper_Benchmark : BenchmarkBase
-    {
-        #region Methods
-        [Benchmark]
-        public void IsProcessHostedByVisualStudio()
-        {
-            var isProcessHostedByVisualStudio = EnvironmentHelper.IsProcessHostedByVisualStudio;
-        }
+//namespace Catel.Benchmarks
+//{
+//    using BenchmarkDotNet.Attributes;
 
-        [Benchmark]
-        public void IsProcessHostedBySharpDevelop()
-        {
-            var isProcessHostedBySharpDevelop = EnvironmentHelper.IsProcessHostedBySharpDevelop;
-        }
+//    public class EnvironmentHelper_Benchmark : BenchmarkBase
+//    {
+//        #region Methods
+//        [Benchmark]
+//        public void IsProcessHostedByVisualStudio()
+//        {
+//            var isProcessHostedByVisualStudio = EnvironmentHelper.IsProcessHostedByVisualStudio;
+//        }
 
-        [Benchmark]
-        public void IsProcessHostedByExpressionBlend()
-        {
-            var isProcessHostedByExpressionBlend = EnvironmentHelper.IsProcessHostedByExpressionBlend;
-        }
+//        [Benchmark]
+//        public void IsProcessHostedBySharpDevelop()
+//        {
+//            var isProcessHostedBySharpDevelop = EnvironmentHelper.IsProcessHostedBySharpDevelop;
+//        }
 
-        [Benchmark]
-        public void IsProcessHostedByTool()
-        {
-            var isProcessHostedByTool = EnvironmentHelper.IsProcessHostedByTool;
-        }
+//        [Benchmark]
+//        public void IsProcessHostedByExpressionBlend()
+//        {
+//            var isProcessHostedByExpressionBlend = EnvironmentHelper.IsProcessHostedByExpressionBlend;
+//        }
 
-        [Benchmark]
-        public void IsProcessCurrentlyHostedByVisualStudio()
-        {
-            var isProcessCurrentlyHostedByVisualStudio = EnvironmentHelper.IsProcessCurrentlyHostedByVisualStudio(true);
-        }
+//        [Benchmark]
+//        public void IsProcessHostedByTool()
+//        {
+//            var isProcessHostedByTool = EnvironmentHelper.IsProcessHostedByTool;
+//        }
 
-        [Benchmark]
-        public void IsProcessCurrentlyHostedBySharpDevelop()
-        {
-            var isProcessCurrentlyHostedBySharpDevelop = EnvironmentHelper.IsProcessCurrentlyHostedBySharpDevelop(true);
-        }
+//        [Benchmark]
+//        public void IsProcessCurrentlyHostedByVisualStudio()
+//        {
+//            var isProcessCurrentlyHostedByVisualStudio = EnvironmentHelper.IsProcessCurrentlyHostedByVisualStudio(true);
+//        }
 
-        [Benchmark]
-        public void IsProcessCurrentlyHostedByExpressionBlend()
-        {
-            var isProcessCurrentlyHostedByExpressionBlend = EnvironmentHelper.IsProcessCurrentlyHostedByExpressionBlend(true);
-        }
+//        [Benchmark]
+//        public void IsProcessCurrentlyHostedBySharpDevelop()
+//        {
+//            var isProcessCurrentlyHostedBySharpDevelop = EnvironmentHelper.IsProcessCurrentlyHostedBySharpDevelop(true);
+//        }
 
-        [Benchmark]
-        public void IsProcessCurrentlyHostedByTool()
-        {
-            var isProcessCurrentlyHostedByTool = EnvironmentHelper.IsProcessCurrentlyHostedByTool(true);
-        }
-        #endregion
-    }
-}
+//        [Benchmark]
+//        public void IsProcessCurrentlyHostedByExpressionBlend()
+//        {
+//            var isProcessCurrentlyHostedByExpressionBlend = EnvironmentHelper.IsProcessCurrentlyHostedByExpressionBlend(true);
+//        }
+
+//        [Benchmark]
+//        public void IsProcessCurrentlyHostedByTool()
+//        {
+//            var isProcessCurrentlyHostedByTool = EnvironmentHelper.IsProcessCurrentlyHostedByTool(true);
+//        }
+//        #endregion
+//    }
+//}
