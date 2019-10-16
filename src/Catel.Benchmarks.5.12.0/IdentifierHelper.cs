2b9a10dc6de9e1a24236748f2e21f820cc60cd76
Geert van Horrik <geert@catenalogic.com>
2019-10-16 22:03:14 +0200

    Add benchmarks for Catel 5.12

---
+++ src/Catel.Benchmarks.5.12.0/IdentifierHelper.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IdentifierHelper.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2015 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Catel.Benchmarks
+{
+    public static partial class IdentifierHelper
+    {
+        private static string GetCatelVersion()
+        {
+            return "5.12.0";
+        }
+    }
+}
