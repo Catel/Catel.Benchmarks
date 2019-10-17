08e72717df70f27de42bffe468e1acebfab67ca8
Geert van Horrik <geert@catenalogic.com>
2019-10-17 09:43:18 +0200

    Decrease benchmark durations

--- src/Catel.Benchmarks.Shared/BenchmarkBase.cs
+++ src/Catel.Benchmarks.Shared/BenchmarkBase.cs
@@ -11,7 +11,7 @@ namespace Catel.Benchmarks
     using BenchmarkDotNet.Engines;
 
     // Uncomment to make all benchmarks slower (but probably more accurate)
-    [SimpleJob(RunStrategy.Throughput, launchCount: 3, warmupCount: 2, targetCount: 10, invocationCount: 5000)]
+    [SimpleJob(RunStrategy.Throughput, launchCount: 3, warmupCount: 2, targetCount: 5, invocationCount: 2500)]
     public abstract class BenchmarkBase
     {
     }
